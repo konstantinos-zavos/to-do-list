fd0ba25e1cdd53b3e12c672f891df00b38812294
koszav <zavos@tuta.io>
2023-02-16 08:34:53 +0200

    Create test factorial function for todos creation

--- src/index.js
+++ src/index.js
@@ -68,3 +68,18 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+const todos = (title, description, dueDate, priority, notes, checklist) => {
+  return { title, description, dueDate, priority, notes, checklist };
+};
+
+const firstTodo = todos(
+  "Morning routine",
+  "Skincare routine, breakfast, make bed",
+  "Every morning 7:00",
+  "P5 (Non-negotiable)",
+  "**notes**",
+  "**checkbox**"
+);
+
+console.table({ firstTodo });
