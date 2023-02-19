b0d43288a15dbda7d36d73eb884a6675578de7d5
koszav <zavos@tuta.io>
2023-02-19 14:26:04 +0200

    Add a form with a button on the html template file to test dom manipulation later

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -8,4 +8,10 @@ function sidebarEnabler() {
   });
 }
 
+function createNewTask() {
+  const title = prompt("Enter the name of the task:");
+  const task = createTask(title);
+  return task;
+}
+
 export { sidebarEnabler };
