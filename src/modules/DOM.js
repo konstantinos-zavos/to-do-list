80173edc52d95abb6d1f70835b90b6b1fbcd0ff2
koszav <zavos@tuta.io>
2023-02-21 12:41:09 +0200

    Add print-to-the-Dom functionality to the print button

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -4,6 +4,9 @@ function testing() {
   defaultProject.content.forEach((element) => console.log(element.title));
   defaultProject.content.forEach((element) => {
     const tester = document.getElementById("content");
+    const sample = document.createElement("div");
+    sample.textContent = element.title;
+    tester.appendChild(sample);
   });
 }
 
