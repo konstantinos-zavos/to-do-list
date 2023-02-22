ed271308fc48cd2db107d5e559756b35f5fa6f9a
koszav <zavos@tuta.io>
2023-02-22 13:49:40 +0200

    Fix bug that would print all the array's objects instead of only the last one

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -1,7 +1,21 @@
 import { defaultProject } from "./projects";
 
 function testing() {
+  const last = defaultProject.content[defaultProject.content.length - 1];
+  console.log(last.title);
+  const tester = document.getElementById("taskcontent");
+  const sample = document.createElement("div");
+  sample.textContent = `Name: ${last.title}`;
+  tester.appendChild(sample);
+  const sample2 = document.createElement("div");
+  sample2.textContent = `Description: ${last.description}`;
+  tester.appendChild(sample2);
+}
+
+function testingOriginal() {
   defaultProject.content.forEach((element) => console.log(element.title));
+  const last = defaultProject.content[defaultProject.content.length - 1];
+  console.log(last.title);
   defaultProject.content.forEach((element) => {
     const tester = document.getElementById("taskcontent");
     const sample = document.createElement("div");
