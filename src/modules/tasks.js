52dd8ffe92bbe7bec5aeca26bf42b6296a32c6da
koszav <zavos@tuta.io>
2023-02-22 13:41:07 +0200

    Add automatic printing of task on page when you create task

--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -1,4 +1,5 @@
 import { defaultProject } from "./projects";
+import { testing } from "./DOM";
 
 const createTask = (title, description, dueDate, priority) => {
   return {
@@ -18,6 +19,7 @@ function createNewTask() {
   console.log(task);
   defaultProject.content.push(task);
   console.log(defaultProject);
+  testing();
   return task;
 }
 
