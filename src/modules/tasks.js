b4230776ca343120e054a9b6ba0169af4f1fd83c
koszav <zavos@tuta.io>
2023-02-24 13:17:33 +0200

    Add support for description aswell

--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -25,7 +25,7 @@ function createNewTask() {
 
 function newTaskTest() {
   const title = document.querySelector("#taskTitleInput").value;
-  const description = prompt("(Optional) Description of the task");
+  const description = document.querySelector("#taskDescriptionInput").value;
   const dueDate = prompt("Due date?");
   const priority = prompt("Priority? (0 to 5)");
   const task = createTask(title, description, dueDate, priority);
