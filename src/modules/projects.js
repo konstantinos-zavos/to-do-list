aa126c5625adafde3e34ab92052eee57251bc791
koszav <zavos@tuta.io>
2023-03-02 09:54:54 +0200

    Create a mew list and populate it with newly created projects

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -3,15 +3,25 @@ const projects = (title) => {
   return { title, content };
 };
 
-const defaultProject = projects("Default project", "0");
+const defaultProject = projects("Default project");
+
+const projectList = () => {
+  const content = [];
+  return { content };
+};
+
+const list = projectList();
 
 function createNewProject() {
   const title = document.querySelector("#projectInput").value;
   const project = projects(title);
   console.log(project.title);
+  list.content.push(project);
+  console.log(list.content);
   return project;
 }
 
 console.log(defaultProject.content);
+console.log(projectList.content);
 
 export { projects, defaultProject, createNewProject };
